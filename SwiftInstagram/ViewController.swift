fd7d268d75e03bd26dc5c4e8bd8406e04294fd4f
Mohammed Zaid Shaikh <zaidshaikh791@gmail.com>
2025-01-31 19:51:48 +0530

    Initial Commit

---
+++ SwiftInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftInstagram
+//
+//  Created by Mohammed Zaid Shaikh on 31/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
